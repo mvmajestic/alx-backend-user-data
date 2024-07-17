c939c1ce1b7c6b251a814f3a0eb6b3161073534e
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-07-17 19:28:18 +0000

    Updated app.py file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,7 +34,7 @@ elif AUTH_TYPE == "session_db_auth":
 @app.before_request
 def bef_req():
     """
-    Will filter each request before it's handled by the proper route
+    Filter each request before it's handled by the proper route
     """
     if auth is None:
         pass
@@ -45,7 +45,7 @@ def bef_req():
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
-        ]
+            ]
         if auth.require_auth(request.path, excluded):
             cookie = auth.session_cookie(request)
             if auth.authorization_header(request) is None and cookie is None:
