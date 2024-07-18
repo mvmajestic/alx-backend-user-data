76213bb69e107aa121bd3b8fc2e7b2c7364d660d
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-07-18 17:40:06 +0000

    Updated app.py file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,118 +2,121 @@
 """
 Flask app
 """
-from flask import (
-    Flask,
-    request,
-    jsonify,
-    abort,
-    redirect,
-    url_for
-)
-
 from auth import Auth
+from flask import Flask, jsonify, request, abort, redirect
 
-app = Flask(__name__)
 AUTH = Auth()
+app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def index() -> str:
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello() -> str:
+    """GET route index
+
+    Returns:
+        str: json {'message': 'Bienvenue'}
     """
-    Return json respomse
-    {"message": "Bienvenue"}
-    """
-    return jsonify({"message": "Bienvenue"})
+    return jsonify({"message": "Bienvenue"}), 200
 
 
-@app.route("/users", methods=["POST"], strict_slashes=False)
-def users() -> str:
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user() -> str:
+    """POST route for user register
+
+    Returns:
+        str: messege
     """
-    Register new users
-    """
-    email = request.form.get("email")
-    password = request.form.get("password")
+    email = request.form.get('email')
+    password = request.form.get('password')
     try:
-        user = AUTH.register_user(email, password)
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"messege": "email already registered"}), 400
 
-    return jsonify({"email": f"{email}", "message": "user created"})
 
-
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    """
-    Log in a user if the credentials provided are correct, and create a new
-    session for them.
-    """
-    email = request.form.get("email")
-    password = request.form.get("password")
+    """login
 
-    if not AUTH.valid_login(email, password):
+    Returns:
+        str: messege
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+    if not valid_login:
         abort(401)
-
     session_id = AUTH.create_session(email)
-    resp = jsonify({"email": f"{email}", "message": "logged in"})
-    resp.set_cookie("session_id", session_id)
-    return resp
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
-@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
-def logout():
-    """
-    Log out a logged in user and destroy their session
-    """
-    session_id = request.cookies.get("session_id", None)
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None or session_id is None:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect("/")
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ logout
 
-
-@app.route("/profile", methods=["GET"], strict_slashes=False)
-def profile() -> str:
+    Return:
+       str: message
     """
-    Return a user's email based on session_id in the received cookies
-    """
-    session_id = request.cookies.get("session_id")
+    session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": f"{user.email}"}), 200
-    abort(403)
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
-@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """profile
+
+    Return:
+       str: message
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
-OBOBOB    """
-    Generate a token for resetting a user's password
-OBOBOB    """
-OBOBOB    email = request.form.get("email")
-    try:
-OBOBOB        reset_token = AUTH.get_reset_password_token(email)
-    except ValueError:
+    """get_reset_password
+
+    Return:
+       str: message
+    """
+    email = request.form.get('email')
+    user = AUTH.create_session(email)
+    if not user:
         abort(403)
-OBOBOB
-OBOBOB    return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"})
-OBOBOB
-OBOBOB
-@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+    else:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": f"{email}", "reset_token": f"{token}"})
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
-    """
-    Update a user's password
-    """
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
+    """update_password
 
+    Return:
+       str: message
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_psw = request.form.get('new_password')
     try:
-        AUTH.update_password(reset_token, new_password)
-    except ValueError:
+        AUTH.update_password(reset_token, new_psw)
+        return jsonify({"email": f"{email}",
+                        "message": "Password updated"}), 200
+    except Exception:
         abort(403)
 
-    return jsonify({"email": f"{email}", "message": "Password updated"})
 
-OBOBOB
 if __name__ == "__main__":
-OBOBOB    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000")
